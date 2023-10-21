31a582edebbbe73e430c1fc2d9e493950ce39288
Harsh Chaudhari <140699920+Harshh212@users.noreply.github.com>
2023-10-22 00:01:09 +0530

    Create 07_19_Harsh.c

---
+++ 07_19_Harsh.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+int isOperator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
+}
+
+int getPrecedence(char operator) {
+    if (operator == '+' || operator == '-') {
+        return 1;
+    } else if (operator == '*' || operator == '/') {
+        return 2;
+    }
+    return 0;
+}
+
+void infixToPostfix(char infix[], char postfix[]) {
+    char stack[MAX_SIZE];
+    int top = -1;
+    int i, j;
+
+    for (i = 0, j = 0; infix[i] != '\0'; i++) {
+        char token = infix[i];
+
+        if (token >= '0' && token <= '9') {
+            postfix[j++] = token;
+        } else if (isOperator(token)) {
+            while (top >= 0 && stack[top] != '(' && getPrecedence(stack[top]) >= getPrecedence(token)) {
+                postfix[j++] = stack[top--];
+            }
+            stack[++top] = token;
+        } else if (token == '(') {
+            stack[++top] = token;
+        } else if (token == ')') {
+            while (top >= 0 && stack[top] != '(') {
+                postfix[j++] = stack[top--];
+            }
+            if (top >= 0 && stack[top] == '(') {
+                top--;
+            }
+        }
+    }
+
+    while (top >= 0) {
+        postfix[j++] = stack[top--];
+    }
+
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX_SIZE], postfix[MAX_SIZE];
+    
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+    
+    infixToPostfix(infix, postfix);
+    
+    printf("The postfix expression is: %s\n", postfix);
+    
+    return 0;
+}
